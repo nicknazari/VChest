b3c1cf7096b2653396d67e0a020164546da77983
Nick Nazari <nhn2915@gmail.com>
2019-01-18 19:44:24 -0500

    updated code

--- src/main/java/io/github/nicknazari/VChest/VChest.java
+++ src/main/java/io/github/nicknazari/VChest/VChest.java
@@ -3,6 +3,7 @@ package io.github.nicknazari.VChest;
 import org.bukkit.Bukkit;
 
 import java.util.HashMap;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -12,16 +13,18 @@ import org.bukkit.plugin.java.JavaPlugin;
 import io.github.nicknazari.DataStorage.DataStorage;
 
 public final class VChest extends JavaPlugin {
+	
+	public static HashMap<Player, Inventory> inventories = DataStorage.getInvenMap();
+	
 	@Override
     public void onEnable() {
     	getLogger().info("VChest 1 ENABLE\n");
+    	getLogger().info("Loading player VChests");
+    	
+    	// load process
+    	
     }
 	
-    @Override
-    public void onDisable() {
-    	getLogger().info("VChest v1 DISABLE\n");
-    }
-    
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
     	
@@ -29,23 +32,29 @@ public final class VChest extends JavaPlugin {
     		
     		Player player = (Player) sender;
     		
-    		HashMap<Player, Inventory> inventories = DataStorage.getInvenMap();
+    		// initialize HashMap to store vchests
+    		// HashMap<Player, Inventory> inventories = DataStorage.getInvenMap();
     		
     		if (!(inventories.containsKey(player))) {
     			Inventory chest = Bukkit.getServer().createInventory(null, 54);
         		inventories.put(player, chest);     		
-    		} else {
-    			player.openInventory(inventories.get(player));
-        		inventories.put(player, inventories.get(player));
     		}
     		
-    		
-    		
+    		player.openInventory(inventories.get(player));
+    		inventories.put(player, inventories.get(player));
     		
     		return true;
     	}
-    		
     	
     	return false;
+    }
+    
+    @Override
+    public void onDisable() {
+    	getLogger().info("Saving player VChests");
+    	
+    	// save process
+    	
+    	getLogger().info("VChest v1 DISABLE\n");
     }
 }
